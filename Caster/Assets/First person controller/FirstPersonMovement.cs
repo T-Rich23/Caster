9557c30746280dd2256eeeb431f91794d1d2f7ef
T-Rich23 <58788425+T-Rich23@users.noreply.github.com>
2021-04-05 15:01:37 -0400

    Orgin Push

---
+++ Caster/Assets/First person controller/FirstPersonMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+    Vector2 velocity;
+    Animator Anim;
+    private void Awake()
+    {
+        Anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        BWalk();
+        Walk();
+    }
+    void Walk()
+    {
+        if (Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.A))
+        { 
+            Anim.SetBool("IsWalking", true); 
+            velocity.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            transform.Translate(velocity.x, 0, velocity.y);
+        }
+        else
+        {
+            Anim.SetBool("IsWalking", false);
+        }
+    }
+    void BWalk()
+    {
+        if ( Input.GetKey(KeyCode.S))
+        {
+            Anim.SetBool("IsBackWalk", true);
+            velocity.y = Input.GetAxis("Vertical") * -speed * Time.deltaTime;
+            velocity.x = Input.GetAxis("Horizontal") * -speed * Time.deltaTime;
+            transform.Translate(-velocity.x, 0, -velocity.y);
+        }
+        else
+        {
+            Anim.SetBool("IsBackWalk", false);
+        }
+    }
+
+   
+}
---
+++ Caster/Assets/Scripts/MagicScript.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicScript : MonoBehaviour
+{
+   [SerializeField]
+    public Transform handEnd;
+    public GameObject baseAtk;
+    public GameObject midAtk;
+    public GameObject heavyAtk;
+    public GameObject ultAtk;
+    public GameObject leftHand, rightHand;
+    [SerializeField]ParticleSystem.MainModule leftHandParticle;
+    [SerializeField]ParticleSystem.MainModule rightHandParticle;
+    public bool canShoot;
+    public float castTime;
+    public float nextCast;
+    GameObject currSpell;
+   
+    public int destroyTime = 3;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        leftHandParticle = leftHand.GetComponent<ParticleSystem>().main;
+        rightHandParticle = rightHand.GetComponent<ParticleSystem>().main;
+        currSpell = baseAtk;
+        canShoot = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cast();
+        CastTime();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currSpell = baseAtk;
+            leftHandParticle.startColor = Color.red;
+            rightHandParticle.startColor = Color.red;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currSpell = midAtk;
+            leftHandParticle.startColor = Color.blue;
+            rightHandParticle.startColor = Color.blue;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currSpell = heavyAtk;
+            leftHandParticle.startColor = Color.cyan;
+            rightHandParticle.startColor = Color.cyan;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            currSpell = ultAtk;
+            leftHandParticle.startColor = Color.magenta;
+            rightHandParticle.startColor = Color.magenta;
+        }
+       
+    }
+
+    public void Cast()
+    {
+        if (Time.time > nextCast)
+        {
+            if (Input.GetMouseButtonDown(0) && canShoot)
+            {
+                GameObject bullet = Instantiate(currSpell, handEnd.position, handEnd.rotation);
+                bullet.GetComponent<Rigidbody>().AddForce(handEnd.forward * 200, ForceMode.Impulse);
+                Destroy(bullet, destroyTime);
+                nextCast = Time.time + castTime;
+            }
+        }
+    }
+    public void CastTime()
+    {
+        if (currSpell == baseAtk)
+        {
+            castTime = .20f;
+        }
+        if (currSpell == midAtk)
+        {
+            castTime = .60f;
+        }
+        if (currSpell == heavyAtk)
+        {
+            castTime = 1f;
+        }
+        if (currSpell == ultAtk)
+        {
+            castTime = 1.4f;
+        }
+    }
+
+}

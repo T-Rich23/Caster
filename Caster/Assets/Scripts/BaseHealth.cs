860d132c159841b76548317da7e4daada98db2f3
T-Rich23 <58788425+T-Rich23@users.noreply.github.com>
2021-04-19 23:14:48 -0400

    Added Base Health
    -Made a base health script
    -With possible respawn capabilities
    -Altered the Dusk prefab (more concentrated effect)

---
+++ Caster/Assets/Scripts/BaseHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHealth : MonoBehaviour
+{
+    [SerializeField]
+    public int maxHealth;
+    public int currentHealth;
+    public Transform respawnMarker = null;
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void ApplyDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+
+            if (respawnMarker)
+            {
+                if (transform.GetComponent<Rigidbody>() != null)
+                    transform.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+                transform.position = respawnMarker.position;
+                currentHealth = maxHealth;
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
